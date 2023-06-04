ebb698e7812341c398a6818d74a21107dc2af4c2
Никита Ковалдов <49562847+N3kitGame@users.noreply.github.com>
2023-06-04 23:35:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,27 +3,27 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
+#include <string>
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-    std::ifstream file(filename);
-    if (!file.is_open()) {
-        throw std::string("ERROR!");
+  std::ifstream file(filename);
+  if (!file.is_open()) {
+    throw std::string("ERROR!");
+  }
+  std::string word = "";
+  BST<std::string> bst;
+  while (!file.eof()) {
+    word = "";
+    while (1) {
+      char symb = file.get();
+      if ((symb >= 65 && symb <= 90) || (symb >= 97 && symb <= 122)) {
+        word += tolower(symb);
+      } else {
+        break;
+      }
     }
-    std::string word = "";
-    BST<std::string> bst;
-    while (!file.eof()) {
-        word = "";
-        while (1) {
-            char symb = file.get();
-            if ((symb >= 65 && symb <= 90) || (symb >= 97 && symb <= 122)) {
-                word += tolower(symb);
-            }
-            else {
-                break;
-            }
-        }
-        bst.AddValue(word);
-    }
-    return bst;
+    bst.AddValue(word);
+  }
+  return bst;
 }
