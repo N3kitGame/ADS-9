acc860c3a395e8af643b85614770b65c90f1e789
Никита Ковалдов <49562847+N3kitGame@users.noreply.github.com>
2023-06-04 23:25:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,23 +7,24 @@
 using namespace std;
 
 BST<std::string> makeTree(const char* filename) {
-  ifstream file(filename);
-  if (!file.is_open()) {
-    throw std::string("ERROR!");
-  }
- string word = "";
- BST<std::string> bst;
-  while (!file.eof()) {
- word = "";
-    while (1) {
-      char symb = file.get();
-      if ((symb >= 65 && symb <= 90) || (symb >= 97 && symb <= 122)) {
- word += tolower(symb);
- } else {
-        break;
-      }
+    ifstream file(filename);
+    if (!file.is_open()) {
+        throw std::string("ERROR!");
     }
- bst.AddValue(word);
-  }
-  return bst;
+    string word = "";
+    BST<std::string> bst;
+    while (!file.eof()) {
+        word = "";
+        while (1) {
+            char symb = file.get();
+            if ((symb >= 65 && symb <= 90) || (symb >= 97 && symb <= 122)) {
+                word += tolower(symb);
+            }
+            else {
+                break;
+            }
+        }
+        bst.AddValue(word);
+    }
+    return bst;
 }
